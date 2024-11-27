d80af5a215f4f5009bd562840d5e823e3adc35f8
JorgeA-NinaT <160784038+JorgeA-NinaT@users.noreply.github.com>
2024-11-27 13:23:44 -0400

    Descripción de los cambios

--- conexion.php
+++ conexion.php
@@ -5,10 +5,10 @@ $user = 'riego_user';
 $password = 'hPZGQbOwfxJeOKbSGQ9IfCl3weGSeTNI';
 
 try {
+    // Conexión con PDO para PostgreSQL
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
     // Establece el modo de errores a excepciones
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos";
 } catch (PDOException $e) {
     echo "Error al conectar a la base de datos: " . $e->getMessage();
 }
